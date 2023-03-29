4bb724fcebb1bf98afa8e80e31d69ebbeb9fc36e
daitech20 <daindp.tech@gmail.com>
2023-03-29 15:06:20 +0700

    create func readfile + writefile

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 # IMPORT LIB
+def read_file(file_name, multidimensional=False):
+    file_data = ""
+    list_data = ""
 
+    with open(file_name) as file:
+        for line in file:
+            file_data += (line.strip())
+        file.close
+
+    list_data = file_data.split("/")
+    
+    for key, data in enumerate(list_data):
+        if data == '':
+            list_data.remove(data)
+        else:
+            list_data[key] = data[len(str(key+1)):].strip()
+    
+    return list_data
+
+def write_file(list_data, file_name, multidimensional=False):
+
+    with open(file_name, 'w') as file:
+        for line in list_data:
+            file.write(line + '\n')
+        
+        file.close
 
 if __name__ == "__main__":
     print("Hello, World!")
+    list_data = read_file("./dataset/test.txt")
+    write_file(list_data, "./dataformat/test.txt")
